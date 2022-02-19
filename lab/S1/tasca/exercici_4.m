506c7cf7ba771732075ac30e651b55f6b9785420
adriaag <99830362+adriaag@users.noreply.github.com>
2022-02-19 13:24:21 +0100

    Add files via upload

---
+++ lab/S1/tasca/exercici_4.m
@@ -0,0 +1,15 @@
+%creació de la figura
+[x, y] = meshgrid(-14:1:15);
+za = -(x.^2+y.^2)/400 + 0.5;
+
+%concatenació
+[x2, y2] = meshgrid(1:1:60);
+z = [za za; za za];
+
+%pintem el plot
+z(z<0)=0;
+colormap jet;
+surf(x2,y2,z);
+
+
+
