a39ff6ccc796d4e304dc39c1392c05ab1820ce88
david latorre <davidlatorremotos@gmail.com>
2022-02-26 12:38:08 +0100

    Update S2.m

--- lab/S2/tasca/S2.m
+++ lab/S2/tasca/S2.m
@@ -2,3 +2,13 @@ clear
 clc
 close all
 
+%% 1
+A = double(imread('_MG_7735.JPG'))/255;
+B = double(imread('_MG_7737.JPG')) /255;
+
+
+%% 2
+DIF = abs(A-B); % imatge diferencia
+maxim = max(DIF(:));
+DIF = DIF/maxim; % dividim pel seu valor màxim
+imshow(DIF);
