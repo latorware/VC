4f19134576a221a28ac97b868ae35c72eea4ff18
david latorre <davidlatorremotos@gmail.com>
2022-03-07 15:53:08 +0100

    a

--- lab/S3/tasca/E3.m
+++ lab/S3/tasca/E3.m
@@ -1,7 +1,7 @@
 %%Exercici 3
 
 %% Càrrega d'imatges
-
+%% Comparacio tres imatges que han de tenir les mateixes dimensions
 I1 = rgb2gray(imread('_61A5845.jpg'));
 I2 = rgb2gray(imread('_61A5855.jpg'));
 I3 = rgb2gray(imread('_61A5861.jpg'));
@@ -16,8 +16,14 @@ I3 = rgb2gray(imread('_61A5861.jpg'));
 %% Filtre Sobel
 
 S1 = Sobel(I1);
+figure
+imshow(S1); 
 S2 = Sobel(I2);
+figure
+imshow(S2); 
 S3 = Sobel(I3);
+figure
+imshow(S3);
 % S4 = Sobel(I4);
 %montage({I1,S1,I2, S2, I3, S3});
 
@@ -30,15 +36,37 @@ figure
 histogram(S3);
 
 %% Histogrames de les imatges sobel (retallades perque nomes hi hagi els pixesl centrals)
-% Subimatges que contenen pixels que estan a una distancia inferior o igual
-% a diagonalImatge/8 respecte el centre de la imatge
-centreImatge = 
+% Subimatges que contenen els 10.000 (100x100) pixels més centrals de cada imatge
+% respectivament
+centreImatgex = double(size(S1, 2))/2.0; 
+centreImatgey = double(size(S1, 1))/2.0;
+S1cropped = imcrop(S1, [(centreImatgex - 50) (centreImatgey - 50) 100 100]); 
+S2cropped = imcrop(S2, [(centreImatgex - 50) (centreImatgey - 50) 100 100]); 
+S3cropped = imcrop(S3, [(centreImatgex - 50) (centreImatgey - 50) 100 100]); 
+figure
+imshow(S1cropped); 
+figure 
+histogram(S1cropped); 
+figure
+imshow(S2cropped); 
+figure
+histogram(S2cropped); 
+figure
+imshow(S3cropped); 
+figure
+histogram(S3cropped); 
+
 
 %% Càlcul mètrica
 
-m1 = Metrica(S1);
-m2 = Metrica(S2);
-m3 = Metrica(S3);
+[m1, M1, enf1] = Metrica(S1);
+[m2, M2, enf2] = Metrica(S2);
+[m3, M3, enf3] = Metrica(S3);
+
+%% gràfica resultats
+x = categorical(["Imatge1", "Imatge2", "Imatge3"]); 
+y = [(m1) (m2) (m3)]; 
+plot(x,y); 
 
 %% Mostra la imatge amb major mètrica
 
