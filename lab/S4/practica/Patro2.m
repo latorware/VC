eb5482b391e89f26ebfa2f1844ca99f32e574e73
david latorre <davidlatorremotos@gmail.com>
2022-03-23 13:42:50 +0100

    a

--- lab/S4/practica/Patro2.m
+++ lab/S4/practica/Patro2.m
@@ -1,12 +1,15 @@
+clear
+close all
+
 %% Llegir imatge
 I = imread('24486978.bmp');
 I = rgb2gray(I);
- imshow(I);
+ %imshow(I);
 
 %% Decisió de llindar
 h = imhist(I);
-figure
-plot(h);
+%figure
+%plot(h);
 
 BW = I > 200;
 figure
@@ -14,15 +17,18 @@ imshow(BW);
 
 %% Patró 2
 
-EE = strel('diamond', 10); 
-A = imerode(BW, EE); 
+EE = strel('square', 24); 
+A = imdilate(BW, EE); 
+figure
+imshow(A)
+EE2 = strel('line', 10, 0); 
+A = imerode(A, EE2); 
 figure
 imshow(A)
 
 
-
 %%
-
+%{
 IMG = imread('24486978.bmp');
 IMG(:,:,1) = IMG(:,:,1) .* uint8(not(FINAL));
 IMG(:,:,3) = IMG(:,:,3).* uint8(not(FINAL));
@@ -30,3 +36,4 @@ IMG(:,:,2) = IMG(:,:,2) + uint8(FINAL)*255;
 
 figure
 imshow(IMG);
+%}
